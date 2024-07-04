7da9824204b595e279d8d27002df0303031e8c81
unknown <kholiqfadhilah2208@gmail.com>
2024-07-04 14:52:40 +0700

    Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import { generate } from "generate-nice-eth-address";
+
+function generateAddressEVM(postfix) {
+    let result = generate("milk");
+    return result;
+}
+
+export { generateAddressEVM };
